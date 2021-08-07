c3ff6252a47b991383415d8720de37dc22761ecc
soniya rana <soniyasonu0703@gmailo.com>
2021-08-07 21:55:35 +0530

    Calculator code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Button from './components/Button';
+import Display from './components/Display';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { content } from './components/content';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	state = {
+		lastPressed: undefined,
+		currentNum: '0',
+		prevNum: undefined,
+		operation: undefined
+	};
+
+	handleClick = (e) => {
+		const { lastPressed, currentNum, prevNum, operation } = this.state;
+		// console.log(e.target.innerText);
+		const { innerText } = e.target;
+
+		if (!Number.isNaN(Number(innerText))) {
+			if (currentNum === '0') {
+				this.setState({
+					currentNum: innerText
+				});
+			} else {
+				this.setState({
+					currentNum: currentNum + innerText
+				});
+      }
+      return;
+		}
+
+		switch (innerText) {
+			case 'AC': {
+				this.setState({
+					currentNum: '0',
+					prevNum: undefined,
+					lastPressed: undefined
+				});
+				break;
+			}
+
+			case '.': {
+				if (!currentNum.includes('.')) {
+					this.setState({
+						currentNum: currentNum + innerText
+					});
+				}
+				break;
+			}
+
+			default: {
+				if (!operation) {
+					this.setState({
+						operation: innerText,
+						prevNum: currentNum,
+						currentNum: 'o'
+					});
+				} else {
+          const evaluated = eval(`${prevNum} ${operation} ${currentNum}`);
+          
+					this.setState({           
+						operation: innerText,
+            prevNum: evaluated,
+            currentNum: '0'
+          });
+          
+          if(innerText === '−'){
+            this.setState({
+              currentNum:evaluated
+            })
+          }
+				}
+			}
+		}
+	};
+	render() {
+		const { currentNum } = this.state;
+		return (
+			<div className='App'>
+				<Header />
+				<Display show={currentNum} />
+				<div className='buttonContainer'>
+					{content.map((num) => (
+						<Button key={num} value={num} onClickButton={this.handleClick} />
+					))}
+				</div>
+				{/*<Buttons key={content} values={content} onClick={this.handleClick} />*/}
+				<Footer />
+			</div>
+		);
+	}
 }
 
 export default App;
